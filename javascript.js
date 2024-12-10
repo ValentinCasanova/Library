cd92faf5b6b742b4dd7f17f1e3fb81831e324335
Valentin <vcasanov@ucsc.edu>
2024-12-10 15:37:01 -0800

    Refactored js file to encapsulate data and functions into Library class

--- javascript.js
+++ javascript.js
@@ -1,91 +1,89 @@
-const myLibrary = [];
 const addBookButton = document.querySelector('.add-book button');
 const dialog = document.querySelector('dialog');
 const submitButton = document.querySelector('#submit');
 dialog.close();
 
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Library {
+    constructor (){
+        this.myLibrary = [];
+    }
 
-Book.prototype.info = function(){
-    let read = this.read ? 'already read' : 'not read yet'
-    let info = `${this.author} by ${this.author}, ${pages} pages, ${read}`;
-    return info;
-}
+    createBook(title, author, pages, read){
+        const readAlr = read ? 'already read' : 'not read yet'
+        const info = `${this.author} by ${this.author}, ${pages} pages, ${readAlr}`;
+        return {title, author, pages, read, info};
+    }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read))
-}
+    addBookToLibrary(title, author, pages, read) {
+        this.myLibrary.push(this.createBook(title, author, pages, read));
+    }
 
-function displayBooks(){
-    const currentLibrary = document.querySelector('.library');
-    const addBook = document.querySelector('.add-book');
-
-    myLibrary.forEach((book) => currentLibrary
-    .insertBefore(createDOMBook(book), addBook));
-}
-
-function displayLatestBook(){
-    const book = myLibrary[myLibrary.length - 1];
-    const currentLibrary = document.querySelector('.library');
-    const addBook = document.querySelector('.add-book');
-    currentLibrary
-    .insertBefore(createDOMBook(book), addBook)
-}
-
-function createDOMBook(book){
-    const newBook = document.createElement('div');
-    newBook.className = `book-card ${book.title}`;
+    displayBooks(){
+        const currentLibrary = document.querySelector('.library');
+        const addBook = document.querySelector('.add-book');
     
-    const deleteButton = document.createElement('button');
-    const deleteText = document.createTextNode('x');
-    deleteButton.appendChild(deleteText);
-    deleteButton.id = 'delete';
+        this.myLibrary.forEach((book) => currentLibrary
+        .insertBefore(this.createDOMBook(book), addBook));
+    }
 
-    deleteButton.addEventListener('click', () =>{
-        newBook.remove();
-    })
+    displayLatestBook(){
+        const book = this.myLibrary[this.myLibrary.length - 1];
+        const currentLibrary = document.querySelector('.library');
+        const addBook = document.querySelector('.add-book');
+        currentLibrary.insertBefore(this.createDOMBook(book), addBook);
+    }
 
-
-    const title = document.createElement('h3');
-    const titleText = document.createTextNode(book.title);
-    title.appendChild(titleText);
+    createDOMBook(book){
+        const newBook = document.createElement('div');
+        newBook.className = `book-card ${book.title}`;
+        
+        const deleteButton = document.createElement('button');
+        const deleteText = document.createTextNode('x');
+        deleteButton.appendChild(deleteText);
+        deleteButton.id = 'delete';
     
-    const author = document.createElement('h4');
-    const authorText = document.createTextNode(book.author);
-    author.appendChild(authorText);
-
-    const pages = document.createElement('p');
-    const pagesText = document.createTextNode(`${book.pages} pages`);
-    pages.appendChild(pagesText);
+        deleteButton.addEventListener('click', () =>{
+            newBook.remove();
+        })
     
-    const read = document.createElement('button');
-    const hasRead = book.read ? 'Read' : 'Unread';
-    read.className = `${book.title}`
-    read.id = hasRead.toLowerCase();
-    const readText = document.createTextNode(`${hasRead}`);
-    read.addEventListener('click', () =>{
-        read.id = read.id == 'unread' ? 'read' : 'unread';
-        let newStatus = new String(read.id);
-        newStatus = newStatus[0].toUpperCase() + newStatus.split('').splice(1, newStatus.length).join('');
-        read.innerText = newStatus;
-    })
-    read.appendChild(readText);
-
-    newBook.appendChild(deleteButton);
-    newBook.appendChild(title);
-    newBook.appendChild(author);
-    newBook.appendChild(pages);
-    newBook.appendChild(read);
-
-    return newBook;
-
+    
+        const title = document.createElement('h3');
+        const titleText = document.createTextNode(book.title);
+        title.appendChild(titleText);
+        
+        const author = document.createElement('h4');
+        const authorText = document.createTextNode(book.author);
+        author.appendChild(authorText);
+    
+        const pages = document.createElement('p');
+        const pagesText = document.createTextNode(`${book.pages} pages`);
+        pages.appendChild(pagesText);
+        
+        const read = document.createElement('button');
+        const hasRead = book.read ? 'Read' : 'Unread';
+        read.className = `${book.title}`
+        read.id = hasRead.toLowerCase();
+        const readText = document.createTextNode(`${hasRead}`);
+        read.addEventListener('click', () =>{
+            read.id = read.id == 'unread' ? 'read' : 'unread';
+            let newStatus = new String(read.id);
+            newStatus = newStatus[0].toUpperCase() + newStatus.split('').splice(1, newStatus.length).join('');
+            read.innerText = newStatus;
+        })
+        read.appendChild(readText);
+    
+        newBook.appendChild(deleteButton);
+        newBook.appendChild(title);
+        newBook.appendChild(author);
+        newBook.appendChild(pages);
+        newBook.appendChild(read);
+    
+        return newBook;
+    }
 }
+
+const myLibrary = new Library();
 
 addBookButton.addEventListener('click', () => dialog.showModal());
 
@@ -98,8 +96,8 @@ submitButton.addEventListener('click', (event) => {
     dialog.close();
     if(title.value != '' && author.value != '' && pages.value != '' && read){
         let readVal = read.value == 'Read' ? true : false;
-        addBookToLibrary(title.value, author.value, pages.value, readVal);
-        displayLatestBook();
+        myLibrary.addBookToLibrary(title.value, author.value, pages.value, readVal);
+        myLibrary.displayLatestBook();
         const book = [title, author, pages];
         book.forEach((elem) => elem.value = '');
         if(read) read.checked = false;
@@ -109,9 +107,9 @@ submitButton.addEventListener('click', (event) => {
     dialog.close();
 })
 
-displayBooks();
-addBookToLibrary('The Stand', 'Stephan King', 1224, true);
-displayLatestBook()
-addBookToLibrary('A Song of Ice and Fire', 'George RR Martin', 600, false);
-displayLatestBook()
+myLibrary.displayBooks();
+myLibrary.addBookToLibrary('The Stand', 'Stephan King', 1224, true);
+myLibrary.displayLatestBook()
+myLibrary.addBookToLibrary('A Song of Ice and Fire', 'George RR Martin', 600, false);
+myLibrary.displayLatestBook()
 
